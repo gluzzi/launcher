72297d6381fced668ab54565ead2f24d0923fa7c
gluzzi <61135399+gluzzi@users.noreply.github.com>
2020-02-18 09:33:22 +0300

    update

--- launcher/js/app.js
+++ launcher/js/app.js
@@ -1,20 +1,22 @@
 $("#beSmart").click(beSmart);
 function beSmart() {
     const launcher = $(".container");
-    const button = $("#beSmart")
+    const button = $("#beSmart");
     const ver = $(".launcher-ver");
+    const dot = $(".dot");
     const dlinks = $(".ln-app-d");
     const links = $(".ln-app-l");
 
     if ($(launcher).css("max-width") === "540px")
     {
         launcher.css("max-width", "1140px");
-        button.text("MOBILE")
-        ver.addClass("centered")
+        button.text("MOBILE");
+        dot.removeClass("smart");
     }
     else
     {
         launcher.css("max-width", "540px");
-        button.text("PC")
+        button.text("PC");
+        dot.addClass("smart");
     }
 }
